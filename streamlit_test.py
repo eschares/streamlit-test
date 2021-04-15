8c669cbfe4640d1fcf8f90817ab8d74069e91c4b
eschares <38474105+eschares@users.noreply.github.com>
2021-04-15 13:43:19 -0500

    Add files via upload

---
+++ streamlit_test.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 22:08:46 2021
+
+@author: eschares
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+    
+st.title('Elsevier 2021 Cancellations')
+st.write("Here's our first attempt at using data to create a table:")
+
+file = "C:\\Users\\eschares\\Desktop\\eschares\\Collection Development\\Elsevier\\2021_cancellations_Unsub_finalizedOct2020\\Unsub_Elsevier_2021_cancellations.csv"
+df = pd.read_csv(file)
+st.write(df.head())
+
+option = st.sidebar.selectbox(
+    'Which number do you like best?',
+     df['subscribed'].unique())
+
+'You selected: ', option
+
+slide = st.sidebar.slider(
+    'How much OA?',
+    min(df['free_instant_usage_percent']),
+    max(df['free_instant_usage_percent']),
+    )
