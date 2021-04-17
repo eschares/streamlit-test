5ec32d8815536bc05f853796d87e7eb2c0bbc192
eschares <38474105+eschares@users.noreply.github.com>
2021-04-17 14:03:37 -0500

    Add files via upload
    Working on file uploader, added logo and name

--- Unsub_test.py
+++ Unsub_test.py
@@ -9,17 +9,35 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import locale
 
-locale.setlocale(locale.LC_ALL, "")
 
-st.title('Unsub Analysis')
-st.write("Here's an attempt to make looking at the Unsub data easier:")
-my_slot1 = st.empty()   #save this spot to fill in later for how many rows get selected
+#st.title('Unsub Extender')
+st.image('unsub_extender.png')
 
 file = "Unsub_Elsevier_2021_cancellations.csv"
-df = pd.read_csv(file)
+uploaded_file = st.sidebar.file_uploader('Upload new .csv file:', type='csv')
+if uploaded_file is not None:
+    file = uploaded_file
 
+df = pd.read_csv(file)
+st.write('Analyzing file "' + str(file) + '"')
+#st.write("Here's an attempt to make looking at the Unsub data easier:")
+
+with st.beta_expander("FAQ"):
+    st.write('''
+             What is this? What do I need? How does it work?
+             
+             <href a=unsub.org>Unsub</href> is a collection analysis tool that helps libraries analyze their journal subscriptions.
+             
+             
+             Step 1: Get an Unsub report and choose export file to .csv
+             ''')
+
+
+my_slot1 = st.empty()   #save this spot to fill in later for how many rows get selected
+
+
+# Sliders and filter
 OA_percent_slider = st.sidebar.slider('OA % between:', min_value=0, max_value=max(df['free_instant_usage_percent']), value=(0,max(df['free_instant_usage_percent'])))
 downloads_slider = st.sidebar.slider('Downloads between:', min_value=0, max_value=max(df['downloads']), value=(0,max(df['downloads'])))
 citations_slider = st.sidebar.slider('Citations between:', min_value=0.0, max_value=max(df['citations']), value=(0.0,max(df['citations'])))
@@ -36,21 +54,31 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(df[filt])
 
+#Report the summary stats of what you selected
 selected_jnls = str(df[filt].shape[0])
 total_jnls = str(df.shape[0])
 cost_sum = df[filt]['subscription_cost'].sum()  #cost of selected journals
 currency_string = "${:,.0f}".format(cost_sum)   #format with $, commas, and no decimal points
 my_slot1.text(selected_jnls + ' rows selected out of ' + total_jnls + ' rows, costing ' + currency_string)
 
-#Altair scatter plot
-chart = alt.Chart(df[filt]).mark_circle().encode(
-    x='downloads',
-    y='citations',
-    color='subscribed',
-    tooltip=['title','downloads','citations','subscription_cost'],
-    ).interactive()
-st.altair_chart(chart, use_container_width=True)
 
+#Altair scatter plot
+dl_vs_cit = alt.Chart(df[filt]).mark_circle(size=75, opacity=0.5).encode(
+    x='downloads:Q',
+    y='citations:Q',
+    color='subscribed:N',
+    tooltip=['title','downloads','citations','authorships','subscription_cost'],
+    ).interactive()
+st.altair_chart(dl_vs_cit, use_container_width=True)
+
+#Altair scatter plot
+dl_vs_auth = alt.Chart(df[filt]).mark_circle(size=75, opacity=0.5).encode(
+    x='downloads',
+    y='authorships',
+    color='subscribed:N',   #Nominal data type
+    tooltip=['title','downloads','citations','authorships','subscription_cost'],
+    ).interactive()
+st.altair_chart(dl_vs_auth, use_container_width=True)
 
 
 
